58e9fb9e201a86f10c3040357531eb87b1951802
Petr Solovyev <petersoloviev123@gmail.com>
2024-12-03 13:33:58 +0300

    Remove obsolete sources

--- wrappers.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "shell.h"
-
-ssize_t getline_w(char **restrict lineptr,
-                  size_t *restrict n,
-                  FILE *restrict stream);
-
-void
-process_token(char* token);
